a25a272ba5aa3de9009da45c693a5f2cb7e50a80
pgm-nicolascnude <nicocnud@student.arteveldehs.be>
2022-05-18 08:03:18 +0200

    added the users schema

---
+++ schemas/UserSchema.ts
@@ -0,0 +1,27 @@
+import { list } from '@keystone-6/core';
+import {
+  text,
+  password,
+} from '@keystone-6/core/fields';
+
+export const UserSchema = list({
+  // Here are the fields that `User` will have. We want an email and password so they can log in
+  // and a name so we can refer to them.
+  fields: {
+    name: text({ validation: { isRequired: true } }),
+    email: text({
+      validation: { isRequired: true },
+      isIndexed: 'unique',
+      isFilterable: true,
+    }),
+    // The password field takes care of hiding details and hashing values
+    password: password({ validation: { isRequired: true } }),
+  },
+  // Here we can configure the Admin UI. We want to show a user's name and posts in the Admin UI
+  ui: {
+    listView: {
+      initialColumns: ['name'],
+    },
+  },
+});
+
