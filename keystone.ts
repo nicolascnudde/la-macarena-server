0a6196e8fd4e4a2b010a32515e0076b3ac52445d
pgm-nicolascnude <nicocnud@student.arteveldehs.be>
2022-05-25 10:15:46 +0200

    changed some settings for deployment

--- keystone.ts
+++ keystone.ts
@@ -17,12 +17,17 @@ import { withAuth, session } from './auth';
 export default withAuth(
   // Using the config function helps typescript guide you to the available options.
   config({
-    server: {
-      port: 3001,
-      cors: {
-        credentials: true,
-      }
+    graphql: {
+      apolloConfig: {
+        introspection: true,
+      },
     },
+    // server: {
+      // port: 3001,
+      // cors: {
+      //   credentials: true,
+      // }
+    // },
     // the db sets the database provider - we're using sqlite for the fastest startup experience
     db: {
       provider: 'postgresql',
